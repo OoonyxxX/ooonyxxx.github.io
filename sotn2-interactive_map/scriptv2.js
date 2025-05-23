ef4b822a94056ea40751cd364ef9743c0c15ac3d
OoonyxxX <89767420+OoonyxxX@users.noreply.github.com>
2025-05-23 22:46:13 +0300

    Add files via upload

---
+++ sotn2-interactive_map/scriptv2.js
@@ -0,0 +1,94 @@
+const mapSize = 8192;
+const mapBorder = 1024;
+const mapWidth = 8192;
+const mapHeight = 8192;
+const mapWidthWX = mapSize + mapBorder + (window.innerWidth / 4);
+const mapHeightHX = mapSize + mapBorder + (window.innerHeight / 4);
+const mapWidthWY = -mapBorder - (window.innerHeight / 4);
+const mapHeightHY = -mapBorder - (window.innerWidth / 4);
+const bounds = [[0, 0], [mapHeight, mapWidth]];
+const boundsHW = [[0, 0], [mapHeightHX, mapWidthWX]];
+const boundstest = [[mapHeight, 0], [0, mapWidth]];
+
+
+const map = L.map('map', {
+  crs: L.CRS.Simple,
+  minZoom: -6,
+  maxZoom: 0,
+  zoomSnap: 0.2,
+  zoomDelta: 0.2,
+  zoom: -6,
+  //maxBounds: [[mapHeightHY, mapWidthWY], [mapHeightHX, mapWidthWX]],
+  //maxBounds: boundsHW,
+  maxBoundsViscosity: 0.5,
+  center: [-8192, 0],
+  zoomControl: true
+});
+
+//const image = L.imageOverlay('WorldMap_NoBack.png', bounds).addTo(map);
+//map.fitBounds([[mapHeightHY, mapWidthWY], [mapHeightHX, mapWidthWX]]);
+
+L.tileLayer('MapTilestest/{z}/{x}/{y}.png', {
+  zoomOffset: 6,
+  minZoom: -6,
+  maxZoom: 0,
+  zoom: -6,
+  tileSize: 256,
+  center: [0, -8192],
+  //bounds: [[-mapHeight, -mapWidth], [2048, 2048]],
+  noWrap: true
+}).addTo(map);
+
+
+
+const layers = {
+  "Crypt": L.layerGroup().addTo(map),
+  "Obelisk": L.layerGroup().addTo(map),
+  "Home": L.layerGroup().addTo(map),
+};
+
+const icons = {
+    Crypticon: L.icon({
+    iconUrl: 'icons/T_Icon_Map_Cryptv2.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32]
+  }),
+    Obeliskicon: L.icon({
+    iconUrl: 'icons/T_Icon_Map_Obelisk.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32]
+  }),
+    PlayerHomeicon: L.icon({
+    iconUrl: 'icons/T_Icon_Map_PlayerHome.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32]
+  }),
+    default: L.icon({
+    iconUrl: 'icons/T_Icon_Map_Generic.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32]
+  })
+};
+
+fetch('markers.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(marker => {
+	  const icon = icons[marker.icon] || icons.default;
+
+      const m = L.marker(marker.coords, { icon })
+        .bindPopup(`<b>${marker.name}</b><br>${marker.description}`);
+
+      if (!layers[marker.category]) {
+        layers[marker.category] = L.layerGroup().addTo(map);
+      }
+
+      layers[marker.category].addLayer(m);
+    });
+
+    L.control.layers(null, layers).addTo(map);
+  });
