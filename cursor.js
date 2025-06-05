df20af555e1c89045b30ad8c08ae010eb5bcea55
OoonyxxX <89767420+OoonyxxX@users.noreply.github.com>
2025-06-05 16:58:32 +0300

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -26,7 +26,6 @@ document.addEventListener('mouseover', e => {
 	customCursor.classList.remove('cursor-base');
     customCursor.classList.add('cursor-base--hover');
 	customCursor.classList.add('scaleup');
-	customCursor.classList.remove('scaledown');
   }
 });
 
