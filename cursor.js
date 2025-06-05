158f0fa79346b2271156a85cc7871a8e734471ab
OoonyxxX <89767420+OoonyxxX@users.noreply.github.com>
2025-06-05 06:47:40 +0300

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -10,20 +10,19 @@ const mapContainer = document.getElementById('map');
 
 // показываем базовый курсор при наведении на карту
 mapContainer.addEventListener('mouseenter', () => {
-  customCursor.classList.add('cursor-base');
   customCursor.classList.remove('cursor-base--hover');
 });
 
 // скрываем кастомный курсор при уходе с карты
 mapContainer.addEventListener('mouseleave', () => {
-  customCursor.classList.remove('cursor-base', 'cursor-base--hover');
+  customCursor.classList.remove('cursor-base--hover');
 });
 
 // активные элементы, для которых нужно показывать анимацию hover
 const activeSelectors = '.leaflet-marker-icon, a, button, .leaflet-popup-close-button';
 
 document.addEventListener('mouseover', e => {
-  if (e.target.closest(activeSelectors)) {
+  if (e.target.closest(activeSelectors) && !e.relatedTarget?.closest(activeSelectors)) {
 	customCursor.classList.remove('cursor-base');
     customCursor.classList.add('cursor-base--hover');
 	customCursor.classList.add('scaleup');
@@ -31,7 +30,7 @@ document.addEventListener('mouseover', e => {
 });
 
 document.addEventListener('mouseout', e => {
-  if (e.target.closest(activeSelectors)) {
+  if (e.target.closest(activeSelectors) && !e.relatedTarget?.closest(activeSelectors)) {
     customCursor.classList.remove('cursor-base--hover');
 	customCursor.classList.remove('scaleup');
 	customCursor.classList.add('scaledown');
