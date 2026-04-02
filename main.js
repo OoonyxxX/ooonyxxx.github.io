04d16fa53d45fc4528676d9add638f0f64c6af1d
OoonyxxX <89767420+OoonyxxX@users.noreply.github.com>
2026-04-02 16:09:12 +0300

    cursor cache fix

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
     cacheMETUIElements();
     cacheWindowMatches();
     cacheLoginModalElements();
+    cacheCursor();
 
     initOptElements();
     initOptToggle();
